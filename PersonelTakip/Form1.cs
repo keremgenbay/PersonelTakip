315c012e393854ac8764fdeb55d48a209bb403dc
KEREM <KEREM@DESKTOP-HI6MQ47>
2023-12-18 22:45:38 +0300

    Add project files.

---
+++ PersonelTakip/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonelTakip
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUserNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))  //İlgili Textbox'a harf girişi engelleme için.
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ PersonelTakip/FrmMain.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PersonelTakip
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPersonel = new System.Windows.Forms.Button();
+            this.btnIs = new System.Windows.Forms.Button();
+            this.btnMaas = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.btnDepartman = new System.Windows.Forms.Button();
+            this.btnPozisyon = new System.Windows.Forms.Button();
+            this.btnIzin = new System.Windows.Forms.Button();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPersonel
+            // 
+            this.btnPersonel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPersonel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPersonel.Location = new System.Drawing.Point(94, 43);
+            this.btnPersonel.Name = "btnPersonel";
+            this.btnPersonel.Size = new System.Drawing.Size(132, 150);
+            this.btnPersonel.TabIndex = 0;
+            this.btnPersonel.Text = "Personel İşlemleri";
+            this.btnPersonel.UseVisualStyleBackColor = false;
+            // 
+            // btnIs
+            // 
+            this.btnIs.BackColor = System.Drawing.Color.IndianRed;
+            this.btnIs.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnIs.Location = new System.Drawing.Point(256, 43);
+            this.btnIs.Name = "btnIs";
+            this.btnIs.Size = new System.Drawing.Size(132, 150);
+            this.btnIs.TabIndex = 1;
+            this.btnIs.Text = "İşler";
+            this.btnIs.UseVisualStyleBackColor = false;
+            // 
+            // btnMaas
+            // 
+            this.btnMaas.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnMaas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMaas.Location = new System.Drawing.Point(413, 43);
+            this.btnMaas.Name = "btnMaas";
+            this.btnMaas.Size = new System.Drawing.Size(132, 150);
+            this.btnMaas.TabIndex = 2;
+            this.btnMaas.Text = "Maaş İşlemleri";
+            this.btnMaas.UseVisualStyleBackColor = false;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnLogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLogOut.Location = new System.Drawing.Point(576, 43);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(132, 150);
+            this.btnLogOut.TabIndex = 6;
+            this.btnLogOut.Text = "Çıkış Yap";
+            this.btnLogOut.UseVisualStyleBackColor = false;
+            // 
+            // btnDepartman
+            // 
+            this.btnDepartman.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnDepartman.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDepartman.Location = new System.Drawing.Point(256, 232);
+            this.btnDepartman.Name = "btnDepartman";
+            this.btnDepartman.Size = new System.Drawing.Size(132, 150);
+            this.btnDepartman.TabIndex = 4;
+            this.btnDepartman.Text = "Departman İşlemleri";
+            this.btnDepartman.UseVisualStyleBackColor = false;
+            // 
+            // btnPozisyon
+            // 
+            this.btnPozisyon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnPozisyon.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPozisyon.Location = new System.Drawing.Point(418, 232);
+            this.btnPozisyon.Name = "btnPozisyon";
+            this.btnPozisyon.Size = new System.Drawing.Size(132, 150);
+            this.btnPozisyon.TabIndex = 5;
+            this.btnPozisyon.Text = "Pozisyon İşlemleri";
+            this.btnPozisyon.UseVisualStyleBackColor = false;
+            // 
+            // btnIzin
+            // 
+            this.btnIzin.BackColor = System.Drawing.Color.DarkOrchid;
+            this.btnIzin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnIzin.Location = new System.Drawing.Point(94, 232);
+            this.btnIzin.Name = "btnIzin";
+            this.btnIzin.Size = new System.Drawing.Size(132, 150);
+            this.btnIzin.TabIndex = 3;
+            this.btnIzin.Text = "İzin İşlemleri";
+            this.btnIzin.UseVisualStyleBackColor = false;
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.BackColor = System.Drawing.Color.Red;
+            this.btnKapat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKapat.Location = new System.Drawing.Point(576, 230);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(132, 150);
+            this.btnKapat.TabIndex = 7;
+            this.btnKapat.Text = "Kapat";
+            this.btnKapat.UseVisualStyleBackColor = false;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnIzin);
+            this.Controls.Add(this.btnMaas);
+            this.Controls.Add(this.btnPozisyon);
+            this.Controls.Add(this.btnIs);
+            this.Controls.Add(this.btnDepartman);
+            this.Controls.Add(this.btnPersonel);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menü";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPersonel;
+        private System.Windows.Forms.Button btnIs;
+        private System.Windows.Forms.Button btnMaas;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Button btnDepartman;
+        private System.Windows.Forms.Button btnPozisyon;
+        private System.Windows.Forms.Button btnIzin;
+        private System.Windows.Forms.Button btnKapat;
+    }
+}
